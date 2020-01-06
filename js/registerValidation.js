74caf37285aa4cbba2c529c5311515b2eb312204
cece-song <59180552+cece-song@users.noreply.github.com>
2020-01-06 15:56:23 +0100

    Add files via upload

--- js/registerValidation.js
+++ js/registerValidation.js
@@ -2,16 +2,21 @@ function check(){
 	var form=document.forms[0];
 	var error="";
 	var str=form.password.value;
-	if(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,10}$/.test(str)){}
+	if(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,10}$/.test(str)){	}
 	else{
-		error="the password does not comply with the standard.\n";
+		document.getElementById("hint").innerHTML="<font color='red'>The password does not comply with the standard.\n</font>";
+		document.getElementById("hint2").innerHTML="<font color='red'>The password does not comply with the standard.\n</font>";
+		document.getElementById("password").value="";
+		document.getElementById("confirmPassword").value="";
+		return false;
+//		error="the password does not comply with the standard.\n";
 		}
 	
-	if (error != "") {
-		
-		var errorText = "Your problem is:\n";
-		errorText = errorText + error;
-		window.alert(errorText);
-		return false;
-		}
+//	if (error != "") {
+//
+//		var errorText = "Your problem is:\n";
+//		errorText = errorText + error;
+//		window.alert(errorText);
+//		return false;
+//		}
 }
