a8b8f79d8c105ba6b82bf01d438ef2e087223008
Spycsh <757407490@qq.com>
2020-01-07 23:35:44 +0100

    tipbox for password

---
+++ js/registerTip.js
@@ -0,0 +1,16 @@
+function display(){
+    document.getElementById("pwdInfoBox").style.display = "block";
+}
+
+function disappear(){
+    document.getElementById("pwdInfoBox").style.display = "none";
+}
+
+function displayConfirmBox(){
+    document.getElementById("cpwdInfoBox").style.display = "block";
+}
+
+function disappearConfirmBox(){
+    document.getElementById("cpwdInfoBox").style.display = "none";
+}
+
